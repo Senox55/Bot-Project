d26ec7a83c0c2b6c29280ea20155e3db6cdef0d7
Senox <blya>
2023-12-06 18:06:17 +0500

    Ai work

--- AiModel.py
+++ AiModel.py
@@ -5,14 +5,14 @@ from sklearn.feature_extraction.text import TfidfVectorizer
 comment_data = pd.read_csv(r'C:\Users\very-\Desktop\Bot Project\Bot-Project\steam_comments.csv', sep=';')
 
 # Комментарии
-X_train = comment_data['cleaned_comment']
+X_train = comment_data['comment']
 
 # Вектор игр
 y_train = comment_data['game_name']
 
 # Обучение модели
 tfidf_vectorizer = TfidfVectorizer()
-X_train_tfidf = tfidf_vectorizer.fit_transform(X_train)
+X_train_tfidf = tfidf_vectorizer.fit_transform(X_train.values.astype('U'))
 rf_clf = KNeighborsClassifier()
 rf_clf.fit(X_train_tfidf, y_train)
 
