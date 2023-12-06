208582c534e996cf6e5dae6d5c462071ed31738b
Senox <blya>
2023-12-06 18:06:28 +0500

    database

--- Parsing.py
+++ Parsing.py
@@ -1,47 +1,72 @@
 import requests
 from bs4 import BeautifulSoup
 import csv
-import json, urllib.request
+
+headers = {
+    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_6) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.1.2 Safari/605.1.15'}
 
 
-# Функция для получения HTML-кода страницы с комментариями к игре на Steam
-def get_html(url):
-    response = requests.get(url)
-    return response.text
+def get_game_review(id, language, page_count_comments):
+    comment_list = []
+
+    cursor = ''
+    i = 0
+    while True:
+        url = f'https://steamcommunity.com/app/{id}/homecontent/'
+        params = {
+            'userreviewsoffset': i * 10,
+            'p': i + 1,
+            'workshopitemspage': i + 1,
+            'readytouseitemspage': i + 1,
+            'mtxitemspage': i + 1,
+            'itemspage': i + 1,
+            'screenshotspage': i + 1,
+            'videospage': i + 1,
+            'artpage': i + 1,
+            'allguidepage': i + 1,
+            'webguidepage': i + 1,
+            'integeratedguidepage': i + 1,
+            'discussionspage': i + 1,
+            'numperpage': 10,
+            'browsefilter': 'toprated',
+            'browsefilter': 'toprated',
+            'appid': id,
+            'appHubSubSection': 10,
+            'l': 'english',
+            'filterLanguage': language,
+            'searchText': '',
+            'forceanon': 1,
+            'maxInappropriateScore': 50,
+        }
+        if i > 0:
+            params['userreviewscursor'] = cursor
+        html = requests.get(url, headers=headers, params=params).text
+        soup = BeautifulSoup(html, 'html.parser')
+        reviews = soup.find_all('div', {'class': 'apphub_Card'})
+
+        if not reviews or i == page_count_comments:
+            break
+
+        comment_section = [review.find('div', {'class': 'apphub_CardTextContent'}) for review in reviews]
+        comment = [''.join(review.find_all(string=True, recursive=False)).strip() for review in comment_section]
+        cursor = soup.find_all('form')[0].find('input', {'name': 'userreviewscursor'})['value']
+        comment_list.extend(comment)
+        i += 1
+
+    return comment_list
 
 
-# Функция для парсинга комментариев к игре на Steam
-def parse_comments(data):
-    comments = []
-    for page in data['reviews']:
-        comment = page['review']
-        comments.append(comment)
-    return comments
-
-
-# Главная функция для получения комментариев и записи их в CSV-файл
-def get_comments(game_id, game_name):
-    all_comments = []
-    cursor = 'AoIIPxIgNne07LsE'
-    for page in range(1, 40):
-        with urllib.request.urlopen(f'https://store.steampowered.com/appreviews/{game_id}?json=1&cursor={cursor}') as url:
-            data = json.load(url)
-            if data['success'] == 8 or cursor == data['cursor']:
-                cursor = page
-                continue
-            print(data)
-            all_comments += parse_comments(data)
-            cursor = data['cursor']
-            print(cursor)
-
-    # Запись результатов в CSV-файл
+def write_comments_in_csv(game_id, game_name, page_count_comments, language='default'):
     with open('steam_comments.csv', 'a', newline='', encoding='utf-8') as file:
         writer = csv.writer(file, delimiter=';')
 
-        for comment in all_comments:
+        for comment in get_game_review(game_id, language, page_count_comments):
             writer.writerow([game_id, game_name, comment])
 
 
-if __name__ == "__main__":
-    get_comments(252490, 'Rust')
+write_comments_in_csv(730, 'Counter-Strike 2', 10)
+write_comments_in_csv(570, 'Dota 2', 10)
+write_comments_in_csv(252490, 'Rust', 10)
+write_comments_in_csv(297000, 'Heroes of Might & Magic III', 10)
+
 
