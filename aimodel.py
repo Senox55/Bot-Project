6ef95ff21829a8a2781c6586ab4042ac5be294e9
Senox <blya>
2023-12-10 19:32:37 +0500

    work

--- aimodel.py
+++ aimodel.py
@@ -5,7 +5,7 @@ from sklearn.model_selection import train_test_split
 from sklearn.model_selection import cross_val_score
 from sklearn.model_selection import GridSearchCV
 
-comment_data = pd.read_csv(r'C:\Users\very-\Desktop\Bot Project\Bot-Project\steam_comments.csv', sep=';')
+comment_data = pd.read_csv(r'C:\Users\very-\Desktop\Bot Project\Bot-Project\cleaned_data.csv', sep=',')
 
 # Комментарии
 X_train = comment_data['comment']
@@ -19,17 +19,20 @@ X_train_tfidf = tfidf_vectorizer.fit_transform(X_train.values.astype('U'))
 X_train, X_val, y_train, y_val = train_test_split(X_train_tfidf, y_train, test_size=0.2, random_state=42)
 
 # Обучение модели
-rf_clf = KNeighborsClassifier(leaf_size=1, n_neighbors=9, weights='distance')
+rf_clf = KNeighborsClassifier()
 rf_clf.fit(X_train, y_train)
-# parametrs = {'n_neighbors': range(10),
-#              'weights': ['uniform', 'distance'],
-#              'leaf_size': range(1, 100, 10)}
-# grid = GridSearchCV(rf_clf, parametrs, cv=5, verbose=3)
-# grid.fit(X_train, y_train)
-# print(grid.best_params_)
+parametrs = {'n_neighbors': range(1, 10, 4),
+             'weights': ['uniform', 'distance'],
+             'leaf_size': range(1, 100, 10)}
+grid = GridSearchCV(rf_clf, parametrs, cv=5, verbose=3)
+grid.fit(X_train, y_train)
+print(grid.best_params_)
+print(grid.best_score_)
 
 # Комментарий пользователя
 user_comment = tfidf_vectorizer.transform([input()])
+
+
 
 # Результат
 print(rf_clf.predict(user_comment))
--- parsing.py
+++ parsing.py
@@ -6,27 +6,26 @@ headers = {
     'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_6) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.1.2 Safari/605.1.15'}
 
 
-def get_game_review(id, language, page_count_comments):
+def get_game_review(id, language, start_page_number, finish_page_number):
     comment_list = []
 
     cursor = ''
-    i = 0
     while True:
         url = f'https://steamcommunity.com/app/{id}/homecontent/'
         params = {
-            'userreviewsoffset': i * 10,
-            'p': i + 1,
-            'workshopitemspage': i + 1,
-            'readytouseitemspage': i + 1,
-            'mtxitemspage': i + 1,
-            'itemspage': i + 1,
-            'screenshotspage': i + 1,
-            'videospage': i + 1,
-            'artpage': i + 1,
-            'allguidepage': i + 1,
-            'webguidepage': i + 1,
-            'integeratedguidepage': i + 1,
-            'discussionspage': i + 1,
+            'userreviewsoffset': start_page_number * 10,
+            'p': start_page_number + 1,
+            'workshopitemspage': start_page_number + 1,
+            'readytouseitemspage': start_page_number + 1,
+            'mtxitemspage': start_page_number + 1,
+            'itemspage': start_page_number + 1,
+            'screenshotspage': start_page_number + 1,
+            'videospage': start_page_number + 1,
+            'artpage': start_page_number + 1,
+            'allguidepage': start_page_number + 1,
+            'webguidepage': start_page_number + 1,
+            'integeratedguidepage': start_page_number + 1,
+            'discussionspage': start_page_number + 1,
             'numperpage': 10,
             'browsefilter': 'toprated',
             'browsefilter': 'toprated',
@@ -38,35 +37,33 @@ def get_game_review(id, language, page_count_comments):
             'forceanon': 1,
             'maxInappropriateScore': 50,
         }
-        if i > 0:
+        if start_page_number > 0:
             params['userreviewscursor'] = cursor
         html = requests.get(url, headers=headers, params=params).text
         soup = BeautifulSoup(html, 'html.parser')
         reviews = soup.find_all('div', {'class': 'apphub_Card'})
 
-        if not reviews or i == page_count_comments:
+        if not reviews or start_page_number == finish_page_number:
             break
 
         comment_section = [review.find('div', {'class': 'apphub_CardTextContent'}) for review in reviews]
         comment = [''.join(review.find_all(string=True, recursive=False)).strip() for review in comment_section]
         cursor = soup.find_all('form')[0].find('input', {'name': 'userreviewscursor'})['value']
         comment_list.extend(comment)
-        i += 1
+        start_page_number += 1
 
     return comment_list
 
 
-def write_comments_in_csv(game_id, game_name, page_count_comments, language='default'):
+def write_comments_in_csv(game_id, game_name, start_page_number, finish_page_number, language='default'):
     with open('steam_comments.csv', 'a', newline='', encoding='utf-8') as file:
         writer = csv.writer(file, delimiter=';')
 
-        for comment in get_game_review(game_id, language, page_count_comments):
+        for comment in get_game_review(game_id, language, start_page_number, finish_page_number):
             writer.writerow([game_id, game_name, comment])
 
 
-write_comments_in_csv(730, 'Counter-Strike 2', 10)
-write_comments_in_csv(570, 'Dota 2', 10)
-write_comments_in_csv(252490, 'Rust', 10)
-write_comments_in_csv(297000, 'Heroes of Might & Magic III', 10)
-
-
+write_comments_in_csv(730, 'Counter-Strike 2', 1, 100)
+write_comments_in_csv(570, 'Dota 2', 1, 100)
+write_comments_in_csv(252490, 'Rust', 1, 100)
+write_comments_in_csv(297000, 'Heroes of Might & Magic III', 1, 100)
